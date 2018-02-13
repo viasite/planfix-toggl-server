fda37dd517a00c184eb6a509062db470d40cae00
Stanislav Popov <popstas@gmail.com>
2018-02-13 23:59:13 +0500

    yml config parse

--- app/main.go
+++ app/main.go
@@ -5,38 +5,35 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/jessevdk/go-flags"
 	"github.com/viasite/planfix-toggl-server/app/rest"
 	"github.com/viasite/planfix-toggl-server/app/client"
 	"github.com/jason0x43/go-toggl"
+	"github.com/jinzhu/configor"
 )
 
 var revision string
 
-var opts struct {
-	SentTag         string      `short:"t" long:"sent-tag" env:"SENT_TAG" description:"" default:"sent"`
-	SmtpHost         string
-	SmtpPort         int
-	SmtpSecure      bool
-	PlanfixAccount  string
-	PlanfixAnaliticName  string
-
-	ApiToken          string
-	WorkspaceId       int
-	SmtpLogin        string
-	SmtpPassword      string
-	EmailFrom       string
-	PlanfixAuthorName  string
-	Dbg            bool          `long:"dbg" description:"debug mode"`
+var Config struct {
+	SentTag             string `env:"SENT_TAG" yaml:"sentTag"`
+	SmtpHost            string `env:"SMTP_HOST" yaml:"smtpHost"`
+	SmtpPort            int    `env:"SMTP_PORT" yaml:"smtpPort"`
+	SmtpSecure          bool   `env:"SMTP_SECURE" yaml:"smtpSecure"`
+	PlanfixAccount      string `env:"PLANFIX_ACCOUNT" yaml:"planfixAccount"`
+	PlanfixAnaliticName string `env:"PLANFIX_ANALITIC_NAME" yaml:"planfixAnaliticName"`
+	ApiToken            string `env:"API_TOKEN" yaml:"apiToken"`
+	WorkspaceId         int    `env:"WORKSPACE_ID" yaml:"workspaceId"`
+	SmtpLogin           string `env:"SMTP_LOGIN" yaml:"smtpLogin"`
+	SmtpPassword        string `env:"SMTP_PASSWORD" yaml:"smtpPassword"`
+	EmailFrom           string `env:"EMAIL_FROM" yaml:"emailFrom"`
+	PlanfixAuthorName   string `env:"PLANFIX_AUTHOR_NAME" yaml:"planfixAuthorName"`
+	Debug               bool   `env:"DEBUG" yaml:"debug"`
 }
 
 func main() {
-	if _, err := flags.Parse(&opts); err != nil {
-		os.Exit(1)
-	}
+	configor.Load(&Config, "config.yml", "config.default.yml")
 	fmt.Printf("planfix-toggl %s\n", revision)
 
-
+	os.Exit(0)
 	sess := toggl.OpenSession("")
 	TogglClient := client.TogglClient{
 		Session: sess,
@@ -59,7 +56,7 @@ func main() {
 	println("report:", string(reportJson))*/
 
 	log.SetFlags(log.Ldate | log.Ltime)
-	if opts.Dbg {
+	if Config.Debug {
 		log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds | log.Lshortfile)
 	}
 
