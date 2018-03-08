aea1a57dfd7d01138d4421c0bc8c496641c38f39
Stanislav Popov <popstas@gmail.com>
2018-03-08 12:29:16 +0500

    feat: windows icon

--- scripts/build-dist.sh
+++ scripts/build-dist.sh
@@ -1,6 +1,12 @@
 #!/bin/env bash
 set -e
 
+# icon generate
+icon_src="assets/icon.ico"
+icon_dest="app/resource.syso"
+[ -e "$icon_dest" ] && rm "$icon_dest" | true
+rsrc -ico "$icon_src" -o "$icon_dest"
+
 rm -rf build
 mkdir -p build/dist
 
@@ -9,15 +15,23 @@ git clone -b gh-pages https://github.com/viasite/planfix-toggl-client.git build/
 
 # set version
 version="$(git tag | grep '^[0-9]\.' | tail -n1)"
-sed -i "s/version string/version string = \"$version\"/g" app/main.go
+from='var version string'
+to="$from = \"$version\""
+sed -i "s/$from\$/$to/g" app/main.go
 
-oses="windows linux darwin"
+# go binaries windows
+gox -output "build/bin/{{.OS}}/planfix-toggl-server" -arch="amd64" -os="windows" github.com/viasite/planfix-toggl-server/app
+[ -e "$icon_dest" ] && rm "$icon_dest" | true
+# go binaries other
+gox -output "build/bin/{{.OS}}/planfix-toggl-server" -arch="amd64" -os="linux darwin" github.com/viasite/planfix-toggl-server/app
 
-# go binaries
-gox -output "build/bin/{{.OS}}/planfix-toggl-server" -arch="amd64" -os="$oses" github.com/viasite/planfix-toggl-server/app
+# unset version
+sed -i "s/$to/$from/g" app/main.go
 
+# remove old builds
 rm -rf "build/archives"
 
+oses="windows linux darwin"
 for os in $oses; do
     dir="build/archives/$os"
     mkdir -p "$dir"
@@ -33,7 +47,7 @@ for os in $oses; do
     cp config.default.yml "$dir"
     cp config.dist.yml "$dir/config.yml"
 
-    # archives at build/dist
+    # archives at build/dist zip
     pushd "$dir"
     zip -5 -r -q "../../dist/planfix-toggl-$os.zip" .
     popd
