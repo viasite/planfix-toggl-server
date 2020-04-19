4e812f0238d39d3bf83e82d425c566f80295f39f
Stanislav Popov <popstas@gmail.com>
2020-04-19 18:54:48 +0500

    chore: remove darwin (macos) from build
    https://github.com/getlantern/systray/issues/34

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,1 +1,7 @@
+go get github.com/mitchellh/gox
+go get github.com/akavel/rsrc # windows icon generate
+
+# linux
+apt install libgtk-3-dev libappindicator3-dev libwebkit2gtk-4.0-dev
+
 npm version
--- scripts/build-dist.sh
+++ scripts/build-dist.sh
@@ -20,10 +20,11 @@ to="$from = \"$version\""
 sed -i "s/$from\$/$to/g" app/main.go
 
 # go binaries windows
-gox -output "build/bin/{{.OS}}/planfix-toggl-server" -arch="amd64" -os="windows" github.com/viasite/planfix-toggl-server/app
+# -ldflags -H=windowsgui - https://github.com/getlantern/systray/issues/48
+gox -output "build/bin/{{.OS}}/planfix-toggl-server" -arch="amd64" -os="windows" -ldflags -H=windowsgui github.com/viasite/planfix-toggl-server/app
 [ -e "$icon_dest" ] && rm "$icon_dest" | true
 # go binaries other
-gox -output "build/bin/{{.OS}}/planfix-toggl-server" -arch="amd64" -os="linux darwin" github.com/viasite/planfix-toggl-server/app
+gox -output "build/bin/{{.OS}}/planfix-toggl-server" -arch="amd64" -os="linux" github.com/viasite/planfix-toggl-server/app
 
 # unset version
 sed -i "s/$to/$from/g" app/main.go
@@ -31,13 +32,17 @@ sed -i "s/$to/$from/g" app/main.go
 # remove old builds
 rm -rf "build/archives"
 
-oses="windows linux darwin"
+oses="windows linux"
 for os in $oses; do
     dir="build/archives/$os"
     mkdir -p "$dir"
 
     # bin
     cp -r build/bin/$os/* "$dir"
+
+    # icon
+    mkdir -p "$dir/assets"
+    cp assets/icon.png "$dir/assets"
 
     # client
     mkdir -p "$dir/docroot"
